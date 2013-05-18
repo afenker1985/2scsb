82216716355f85c8e86ca3dcb427c8795e167f25
Aaron Fenker <aaronfenker@gmail.com>
2013-05-17 22:11:18 -0400

    Fixed CSS issue on Dulcem Melodies Album Page

--- php/Albums/dm.php
+++ php/Albums/dm.php
@@ -2,9 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <?
 	$albums = array( 1 => array("DM", "Dulcem Melodies") );
-	include "../header.php";
-	include "../footer.php";
-	include "../getalbum.php";
+	include "http://2scsbfan.info/header.php";
+	include "http://2scsbfan.info/footer.php";
+	include "http://2scsbfan.info/getalbum.php";
 	
 	$content = <<<EOHTML
 	{$header}
